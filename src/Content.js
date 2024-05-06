2d021631a7f35b949ff6acc5c397bef7101bf683
MeatBallFreak69 <glitchclick@gmail.com>
2024-05-06 11:42:27 -0400

    Fixed annoying scroll-bar bug!

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,24 @@
+import Panel from "./Panel";
+import lady from './Images/lady.jpg'
+import instructor from './Images/instructor.png'
+
 const Content = () =>{
     return(
         <main>
-
+            <Panel 
+                layout={0}
+                image={lady}
+                altText={"Lady meditating in the glowing sun"}
+                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+                bgColour={""}
+            />
+            <Panel
+                layout={1}
+                image={instructor}
+                altText={"asds"}
+                text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}
+                bgColour={"#fff"}
+            />
         </main>
     )
 }
--- src/Panel.js
+++ src/Panel.js
@@ -1,7 +1,30 @@
-const Panel = () =>{
-    return(
-        <div className="panel">
+import './Panel.css'
 
+const Panel = ({
+    layout, image, altText, text, bgColour
+}) =>{
+    //Sets background color css to be the prop
+    const css = {backgroundColor: bgColour};
+    //Adjusts the className of the panel based off the layout prop
+    let panelType;
+    switch(layout){
+        case 0:
+            panelType = "panel0"
+            break;
+        case 1:
+            panelType = "panel1"
+            break;
+        default:
+    }
+
+    return(
+        //Added some default values for each panel type with
+        //generalPanel
+        <div className={"generalPanel" + " " + panelType} style={css}>
+            <div className='imgContainer'>
+                <img src={image} alt={altText} />
+            </div>
+            <p>{text}</p>
         </div>
     )
 }
