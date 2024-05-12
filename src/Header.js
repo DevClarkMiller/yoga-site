951b5c3b576ac0dadcd241dfafab34d408e767e3
Clark Miller <63131937+MeatBallFreak69@users.noreply.github.com>
2024-05-12 00:13:07 -0400

    Added burger menu feature for most part, just need to make lined up properly.

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useRef, useContext, useEffect } from 'react'
+import { useRef, useContext, useEffect, useState } from 'react'
 import { RefContext } from './App'
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -13,21 +13,32 @@ const Header = () =>{
     //Still dunno why I do this, just makes it work
     const navigate = useNavigate();
     const context = useContext(RefContext);
+    const navRef = useRef();
+
+    const [isMenuActive, setIsMenuActive] = useState(false);
 
     const {topRef, aboutRef, contactRef, datesRef, scrollTo} = context;
+
+    useEffect(() =>{
+        (isMenuActive) ? navRef.current.classList.add('navActive') : navRef.current.classList.remove('navActive');
+    }, [isMenuActive])
+
+    const burgerClick = () =>{
+        setIsMenuActive(!isMenuActive);
+    }
+
     return(
         <header>
-            <nav>
+            {/*NOTES - MAKE THE BURGER ICON THE ONLY PART OF NAV VISIBLE*/}
+            <nav ref={navRef}>
                 <ul>
+                    <li><GiHamburgerMenu className="burgerMenu" onClick={burgerClick}/></li>
                     <li><a onClick={() => scrollTo(topRef)}>Top</a></li>
                     <li><a onClick={() => scrollTo(aboutRef)}>About</a></li>
                     <li><a onClick={() => scrollTo(contactRef)}>Contact Me</a></li>
                     <li><a onClick={() => scrollTo(datesRef)}>Dates</a></li>
                 </ul>
             </nav>
-
-            {/*NOTES  - HAVE BURGER MENU BUTTON SLIDE NAV OPTIONS OUT*/}
-            <GiHamburgerMenu />
         </header>
     );
 }
