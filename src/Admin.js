4e82a1f771b97641081a537a174c55c2e80f6b2a
MeatBallFreak69 <glitchclick@gmail.com>
2024-05-18 07:46:58 -0400

    Added some more config for the content part

--- src/Admin.js
+++ src/Admin.js
@@ -40,6 +40,9 @@ const Admin = () =>{
     const [editFooterColour, setEditFooterColour] = useState('');
 
     const [editContent, setEditContent] = useState();
+    const [editContentP1, setEditContentP1] = useState(contentConfig.firstPanel);
+    const [editContentP2, setEditContentP2] = useState(contentConfig.secondPanel);
+    const [editContentP3, setEditContentP3] = useState(contentConfig.thirdPanel) 
 
 
     //Will only do this if the datesHeader and datesFooter objects are loaded
@@ -209,17 +212,60 @@ const Admin = () =>{
 
                 <Route path="/content" element={ 
                      <form className="adminForm"> 
-                        <AdminTable tableRows=
-                            {[
-                                {isTArea: false, th: "Username", _type: "text", id: "username", val:username, change: setUsername, labelTxt: "Username"},
-                                {isTArea: false, th: "Password", _type: "password", id: "password", val:password, change: setPassword, labelTxt: "Password"},
-                                {isTArea: false, th:"Title", _type: "text", id: "title", val:editTitle, change: setEditTitle, labelTxt: "Title"},
-                                {isTArea: false, th: "subtitle", _type: "text", id: "subtitle", val:editSubtitle, change: setEditSubtitle, labelTxt:"Subtitle"},
-                                {isTArea: true, th: "Description", _type: "text", id: "description", val:editDescription, change: setEditDescription, labelTxt:"Description"},
-                                {isTArea: false, th: "Org Name", _type: "text", id: "orgName", val:editOrgName, change: setEditOrgName, labelTxt: "Org"},
-                                {isTArea: false, th: "Location", _type: "text", id: "location", val:editLocation, change: setEditLocation, labelTxt: "Location"},
-                                {isTArea: false, th: "Fee", _type: "text", id: "fee", val:editFee, change: setEditFee, labelTxt: "Fee"},
-                            ]} />
+                     <table className="headerFooterTable">
+                            <thead>
+                                <tr>
+                                    <th>Options</th>
+                                    <th>Panel 1</th>
+                                    <th>Panel 2</th>
+                                    <th>Panel 3</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>Layout</th>
+                                    <td><select onChange={(e) => setEditContentP1({...editContentP1, layout: e.target.value})} value={editContentP1.layout ?? 0}>
+                                        <option value={0}>Layout 1</option>
+                                        <option value={1}>Layout 2</option>
+                                    </select></td>
+                                    <td><select onChange={(e) => setEditContentP2({...editContentP2, layout: e.target.value})} value={editContentP2.layout ?? 1}>
+                                        <option value={0}>Layout 1</option>
+                                        <option value={1}>Layout 2</option>
+                                    </select></td>
+                                    <td><select onChange={(e) => setEditContentP3({...editContentP3, layout: e.target.value})} value={editContentP3.layout ?? 1}>
+                                        <option value={0}>Layout 1</option>
+                                        <option value={1}>Layout 2</option>
+                                    </select></td>
+                                    
+                                </tr>
+                                <tr>
+                                    <th>Title</th>
+                                    <td><label className="hide" htmlFor="p1Header">Panel 1 Header</label><input value={editContentP1.header} onChange={(e) => setEditContentP1({...editContentP1, header: e.target.value})} id="p1Header" type="text"></input></td>
+                                    
+                                    <td><label className="hide" htmlFor="p2Header">Panel 2 Header</label><input value={editContentP2.header} onChange={(e) => setEditContentP2({...editContentP2, header: e.target.value})} id="p2Header" type="text"></input></td>
+
+                                    <td><label className="hide" htmlFor="p3Header">Panel 3 Header</label><input value={editContentP3.header} onChange={(e) => setEditContentP3({...editContentP3, header: e.target.value})} id="p3Header" type="text"></input></td>
+                                </tr>
+                                <tr>
+                                    <th>Paragraph</th>
+                                    <td><label className="hide" htmlFor="p1Paragraph">Panel 1 Paragraph</label><textarea value={editContentP1.text} onChange={(e) => setEditContentP1({...editContentP1, text: e.target.value})} id="p1Paragraph" type="text"></textarea></td>
+
+
+                                    <td><label className="hide" htmlFor="p2Paragraph">Panel 2 Paragraph</label><textarea value={editContentP2.text} onChange={(e) => setEditContentP2({...editContentP2, text: e.target.value})} id="p2Paragraph" type="text"></textarea></td>
+
+                                    <td><label className="hide" htmlFor="p3Paragraph">Panel 3 Paragraph</label><textarea value={editContentP3.text} onChange={(e) => setEditContentP3({...editContentP3, text: e.target.value})} id="p3Paragraph" type="text"></textarea></td>
+
+                                </tr>
+                                <tr>
+                                    <th>Colour</th>
+                                    <td><label className="hide" htmlFor="p1Colour">Panel 1 Colour</label><input value={editContentP1.colour} onChange={(e) => setEditContentP1({...editContentP1, colour: e.target.value})} id="p1Colour" type="color"></input></td>
+
+
+                                    <td><label className="hide" htmlFor="headerColour">Header Colour</label><input value={editHeaderColour} onChange={(e) => setEditHeaderColour(e.target.value)} id="headerColour" type="color"></input></td>
+                                    <td><label className="hide" htmlFor="footerTimes">Footer Colour</label><input value={editFooterColour} onChange={(e) => setEditFooterColour(e.target.value)} id="footerColour" type="color"></input></td>
+                                </tr>
+                            </tbody>
+                        </table>
                         <button className="adminBtn" type="submit">{
                         (loading) ? <Oval /> : (submit) ? <BsArrowDownSquareFill /> : <BsArrowDownSquare />}
                         </button> 
