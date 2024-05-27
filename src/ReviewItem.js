b332ff93fc5713ca376d7f34bc31031963d4e616
Clark Miller <63131937+MeatBallFreak69@users.noreply.github.com>
2024-05-27 14:54:08 -0400

    Changed the reviewItems to be a glassmorphic view

--- src/ReviewItem.js
+++ src/ReviewItem.js
@@ -2,7 +2,7 @@ const ReviewItem = ({review}) =>{
     return(
         <div className="reviewItem">
                 <h2>{review.sender}</h2>
-                <p>{review.message}</p>
+                <p>"{review.message}"</p>
         </div>
     )
 }
